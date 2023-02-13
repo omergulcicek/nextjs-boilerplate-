2ea9f4cdecc23ceb714fbb1d707798642cc0586b
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-02-14 01:39:22 +0300

    @next/font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,15 @@
+import { Inter } from "@next/font/google"
 import "./globals.css"
+
+const inter = Inter({
+  weight: ["200", "400", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className={inter.className}>
       <body>{children}</body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    </>
+  )
 }
 
 export const metadata = {
