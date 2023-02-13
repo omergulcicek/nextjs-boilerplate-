e55844a9849147ccc02e4f8cc03786a2e30c38c2
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-02-14 00:56:57 +0300

    SEO metatag

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,8 @@
 export default function Page() {
   return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
 }
+
+export const metadata = {
+  title: "Home",
+  description: "Welcome to Next.js",
+}
