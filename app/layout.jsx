4b39e4fd6ba40d057e71f7b9617dac29523a75c9
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-02-14 03:01:03 +0300

    meta tags

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,8 +9,14 @@ const inter = Inter({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr" className={inter.className}>
+    <html lang="tr" dir="ltr" className={inter.className}>
       <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body>{children}</body>
