ca5bcddf48889f37851867a1e504b5ee98c63025
Ömer Gülçiçek <iletisim@omergulcicek.com>
2023-02-24 21:22:50 +0300

    recoil

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
-import { Header, Footer } from "@/components"
+"use client"
+
+import { RecoilRoot } from "recoil"
 import { Inter } from "@next/font/google"
+import { Header, Footer } from "@/components"
 import "./globals.css"
 
 const inter = Inter({
@@ -10,21 +13,23 @@ const inter = Inter({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr" dir="ltr" className={inter.className}>
-      <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </head>
-      <body className="flex min-h-screen flex-col">
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <RecoilRoot>
+      <html lang="tr" dir="ltr" className={inter.className}>
+        <head>
+          <meta charset="utf-8" />
+          <meta http-equiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </head>
+        <body className="flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </RecoilRoot>
   )
 }
